91e9c29b1a89638ab5d9d090ddecc4a4cddbc06c
jpfrigeriogithub <jpfrigerio@gmail.com>
2020-01-23 00:08:54 -0500

    next one

--- sparktest/src/main/java/frc/robot/Robot.java
+++ sparktest/src/main/java/frc/robot/Robot.java
@@ -9,6 +9,13 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.CANEncoder;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+
 /**
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the TimedRobot
@@ -21,8 +28,24 @@ public class Robot extends TimedRobot {
    * This function is run when the robot is first started up and should be used
    * for any initialization code.
    */
+
+  XboxController controller1 = new XboxController(0);
+     
+  private CANEncoder m_encoder;
+  private CANSparkMax m_motor;
+
+
+
   @Override
   public void robotInit() {
+
+    m_motor = new CANSparkMax(10, MotorType.kBrushless);
+    m_motor.restoreFactoryDefaults();
+    m_encoder = m_motor.getEncoder();
+
+
+
+
   }
 
   @Override
@@ -39,7 +62,18 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopPeriodic() {
+  
+    double setvalue = controller1.getRawAxis(1);
+    SmartDashboard.putNumber("axis", setvalue);
+    m_motor.set(setvalue);
+  //  m_motor.set(.5);
+
+SmartDashboard.putNumber("Encoder Position", m_encoder.getPosition());
+SmartDashboard.putNumber("Encoder Velocity", m_encoder.getVelocity());
+  
   }
+
+
 
   @Override
   public void testInit() {
